8b49aecf275d4ddb84fe3e7334b3430b7187f7fd
eseag <agadagbaesefaith@gmail.com>
2022-06-25 14:34:35 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+let Name = 'Eseoghene'
+console .log (Name)
+
+let height = '5.10'
+console .log (height)
+
+let country = 'Nigeria'
+console.log (country)
+
+document.write(Name+'<br>'+height+'<br>'+ country)
